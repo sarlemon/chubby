203db29d49f942ecab9d27485edc59582d89a72c
sarlemon <406401437@qq.com>
2021-03-03 16:01:08 +0800

    下拉菜单开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Alert from './components/Alert/Alert'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 //text
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={1} onSelect={(index) => { alert(index) }} mode='vertical'>
-          <MenuItem index={1}>
+        <Menu defaultIndex='0' onSelect={(index) => { alert(index) }} mode='vertical' defaultOpenSubMenus={['2']}>
+          <MenuItem>
             cool link
           </MenuItem>
-          <MenuItem index={2} disabled>
+          <MenuItem disabled>
             cool link2
           </MenuItem>
-          <MenuItem index={3}>
+          <SubMenu title="dropdown">
+            <MenuItem>dropdown 1</MenuItem>
+            <MenuItem>dropdown 2</MenuItem>
+          </SubMenu>
+          <MenuItem>
             cool link3
           </MenuItem>
-          <li>asdfasd</li>
         </Menu>
-        <Button>Hello</Button>
-        <Button size={ButtonSize.Large} disabled>Disabled</Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small} className='klass'>Hello</Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu</Button>
-        <Alert type='success' message='hello' description='talsdkjflajf'/>
-        <Alert type='warning' message='hello' description='talsdkjflajf' />
-        <Alert type='danger' message='hello' description='talsdkjflajf' closable />
-        <Alert message='hello' description='talsdkjflajf' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

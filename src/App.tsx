6cd1ed503fb3df72c225feef96c2a6f512cd97a1
akio <406401437@qq.com>
2021-03-05 01:26:09 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
 import SubMenu from './components/Menu/subMenu'
+import Icon from './components/Icon/icon'
+library.add(fas);
 //text
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FontAwesomeIcon icon={faCoffee} size="10x" />
+        <Icon icon="coffee" size="10x" theme="danger" />
         <Menu
           defaultIndex="0"
           onSelect={(index) => {

ab2adf766a34c0a07692ed02b0e192074cd185b1
akio <406401437@qq.com>
2021-03-10 00:51:52 +0800

    storybook引入

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 
+import { configure, addDecorator } from '@storybook/react'
+// import React from 'react'
+import "../src/styles/index.scss"
+configure(require.context('../src', true, /\.stories\.tsx$/), module);
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// const styles: React.CSSProperties = {
+//     textAlign: 'center'
+// }
+
+// const CenterDecorator = (storyFn: any) => <div style={styles}>{storyFn()}</div>
+// addDecorator(CenterDecorator);

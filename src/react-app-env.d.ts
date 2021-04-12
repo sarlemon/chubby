2b7e09c6bba71795fa2c34b12adff8ace6c5ca3a
sarlemon <406401437@qq.com>
2021-04-12 18:02:12 +0800

    webpack配置化

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,4 @@
-// <reference types="react-scripts" />
-declare module "react/jsx-runtime" {
-  export default any;
-}
+/// <reference types="react-scripts" />
+// declare module "react/jsx-runtime" {
+//   export default any;
+// }
